08a7b33158a127ae888840b0450e65f0774443db
shrirammehtasrm <shrirammehtasrm@gmail.com>
2024-06-17 15:28:51 +0530

    team

--- src/pages/common/team/Team.jsx
+++ src/pages/common/team/Team.jsx
@@ -1,30 +1,30 @@
-import { Link } from 'react-router-dom';
-import Breadcrumb from '../../../components/breadcrumb/Breadcrumb';
-import data from './data.json';
-import Member from '../../../components/widget/team/Member';
+import { Link } from "react-router-dom";
+import Breadcrumb from "../../../components/breadcrumb/Breadcrumb";
+import data from "./data.json";
+import Member from "../../../components/widget/team/Member";
 
 const Team = () => {
   return (
     <>
-      <main className='main-wrapper relative overflow-hidden'>
+      <main className="main-wrapper relative overflow-hidden">
         {/*...::: Breadcrumb Section Start :::... */}
-        <Breadcrumb title='Our Team' />
+        <Breadcrumb title="Our Team" />
         {/*...::: Breadcrumb Section End :::... */}
         {/*...::: Team Section Start :::... */}
-        <section id='team-section'>
+        <section id="team-section">
           {/* Section Spacer */}
-          <div className='pb-40 xl:pb-[220px]'>
+          <div className="pb-40 xl:pb-[220px]">
             {/* Section Container */}
-            <div className='global-container'>
+            <div className="global-container">
               {/* Section Content Block */}
-              <div className='jos mb-10 text-center lg:mb-16 xl:mb-20'>
-                <div className='mx-auto md:max-w-xs lg:max-w-xl xl:max-w-[746px]'>
+              <div className="jos mb-10 text-center lg:mb-16 xl:mb-20">
+                <div className="mx-auto md:max-w-xs lg:max-w-xl xl:max-w-[746px]">
                   <h2>Our team consists of a group of talents</h2>
                 </div>
               </div>
               {/* Section Content Block */}
               {/* Team Member List */}
-              <ul className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+              <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                 {/* Team Member Item */}
                 {data.map((member, index) => (
                   <Member key={index} {...member} />
@@ -32,7 +32,7 @@ const Team = () => {
                 {/* Team Member Item */}
 
                 {/* Inviting */}
-                <li
+                {/* <li
                   className='jos min-h-[400px] rounded-[20px] bg-colorLinenRuffle p-[20px]'
                   data-jos_animation='flip'
                   data-jos_delay='0.9'
@@ -52,7 +52,7 @@ const Team = () => {
                       Join our team
                     </Link>
                   </div>
-                </li>
+                </li> */}
               </ul>
             </div>
             {/* Section Container */}
