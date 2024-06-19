692ab08c516c6885c825f22dfd5f9ee16c736c21
shrirammehtasrm <shrirammehtasrm@gmail.com>
2024-06-19 06:57:20 +0530

    check

--- src/constants/baseURL.js
+++ src/constants/baseURL.js
@@ -1,3 +1,3 @@
-export const baseURL = "http://66.29.136.199:9001/api";
+export const baseURL = "http://66.29.136.199:81/api";
 // export default process.env.API_URL + "/api";
 // export const baseURL = "http://localhost:5000/api";
