57c6ff38cedadafabdb790ed13289a1876da91c5
shrirammehtasrm <shrirammehtasrm@gmail.com>
2024-06-21 11:49:40 +0530

    allowqed investment and transfer

--- src/pages/dashboard/Home.jsx
+++ src/pages/dashboard/Home.jsx
@@ -212,7 +212,7 @@ export default function Home() {
                     {allowedTransferId === user?.user?.userId && (
                       <button
                         className="rounded-full w-full px-4 py-2.5 w-full bg-gradient-to-l from-[#8011E8] to-[#CD6AFB] text-white text-base font-normal disabled:bg-gray-900 "
-                        // onClick={() => setIsTransferModalOpen(true)}
+                        onClick={() => setIsTransferModalOpen(true)}
                       >
                         <div className="flex flex-row justify-center gap-4 w-full items-center ">
                           <div className="">Transfer</div>
